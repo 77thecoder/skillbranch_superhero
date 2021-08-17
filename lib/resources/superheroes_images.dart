debb660e3c04d7a4ca56cb22d7dccdc387432e8e
DKostin <DKostin@nvbs.ru>
2021-08-17 11:53:20 +0400

    Добавил файл с картинками

---
+++ lib/resources/superheroes_images.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class SuperheroesImages {
+  const SuperheroesImages._();
+
+  static final hulk = Image.asset('assets/images/hulk.png');
+  static final batman = Image.asset('assets/images/batman.png');
+  static final superman = Image.asset('assets/images/superman.png');
+}
